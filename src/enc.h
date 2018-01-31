f74b819b78ad94a8011ef182274c4a9899a7668a
pommen <pommengt@gmail.com>
2018-01-31 19:58:49 +0100

    Börjar ta form

---
+++ src/enc.h
@@ -0,0 +1,57 @@
+
+int motor1_Enc_A = PA6;
+int motor1_Enc_B = PA7;
+int rot_EncA = PA0;
+int rot_EncB = PA1;
+int rot_EncBTN = PA2;
+
+
+
+int motor2_Enc_A = PA3;
+int motor2_Enc_B = PA4;
+
+volatile int enc_Motor1 =0;
+volatile int rot_enc =0;
+volatile int enc_Motor2 =0;
+volatile int enc_Motor2_OLD =-12;
+
+void Motor1_enc_ISR(){
+
+        //  int m1_Enc_A = digitalRead(motor1_Enc_A);
+        int m1_Enc_B = digitalRead(motor1_Enc_B);
+
+        if (m1_Enc_B == 0) {
+                enc_Motor1--;
+        }
+        if (m1_Enc_B == 1) {
+                enc_Motor1++;
+        }
+
+
+
+}
+void Motor2_enc_ISR(){
+        enc_Motor2_OLD = enc_Motor2;
+        //  int m1_Enc_A = digitalRead(motor1_Enc_A);
+        int m2_Enc_B = digitalRead(motor2_Enc_B);
+
+        if (m2_Enc_B == 0) {
+                enc_Motor2--;
+        }
+        if (m2_Enc_B == 1) {
+                enc_Motor2++;
+        }
+}
+
+void Rot_enc_ISR(){
+
+        int rot_EncB_Value = digitalRead(rot_EncB);
+
+        if (rot_EncB_Value == 0) {
+                rot_enc--;
+        }
+        if (rot_EncB_Value == 1) {
+                rot_enc++;
+        }
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,235 +1,239 @@
+/*
+   I2C addresses:
+
+   LCD:0x3F (63)
+   DAC:0x62 (98)
+
+   todo's:
+
+   meny system:{
+   step back efter körning
+   Current limit
+   Hastighet (PWM)
+
+   }
+
+   LED?
+   Motor divare fram och bak (Servo?)
+   command input (anväanda samma som den gamla?)
+
+
+ */
+
+
+
+
+
+
+
 #include <Arduino.h>
-#include "SSD1306Ascii.h"
-#include "SSD1306AsciiAvrI2c.h"
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            12
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      1
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-#define I2C_ADDRESS 0x3C
-SSD1306AsciiAvrI2c oled;
-
-
-//PINS:
-
-const int in1 = 8;
-const int in2 = 4;
-const int en = 10;
-const int led = 13;
-const int cmd = 5;
-//VARS:
-int del = 3;
-int pulses = 0;
-boolean gotCMD = false;
-int speed =0;
-int distance =0;
-int run_once = 0;
-volatile int pos = 0;
-
-//LED colors:
-
-// Define the color codes by name to make it easier to call later on
-uint32_t red = pixels.Color(255, 0, 0);
-uint32_t green = pixels.Color(0, 255, 0);
-uint32_t yellow = pixels.Color(255, 100, 0);
-
-
-//heartBeat Vars:
-int beat = 0;
-boolean hearbeatDown = false;
-long heartBeatTimer =0;
-int heartBeatMax = 130;
-
-//functions
-void  off();
-void fram();
-void bak();
-void run(int steps);
-void count_pulses();
-void enc();
-void heartBeat();
-
-
-void setup() {
-
-        Serial.begin(115200);
-        //init Rotary Encoder
-        Serial.println("Init Rotary Encoder...");
-        oled.begin(&Adafruit128x32, I2C_ADDRESS);
-        oled.setFont(Adafruit5x7);
-        pixels.begin(); // This initializes the NeoPixel library.
-        pixels.setBrightness(32);
-
-
-        pinMode(in1, OUTPUT);
-        pinMode(in2, OUTPUT);
-        pinMode(en, OUTPUT);
-        pinMode(cmd, INPUT);
-        detachInterrupt(0);
-        oled.clear();
-        //first row
-        oled.println("Dispenser");
-
-
-                for (size_t i = 0; i < 50; i++) {
-                        pixels.setPixelColor(0, pixels.Color(-i,i,-i)); // Moderately bright green color.
-                        pixels.show(); // This sends the updated pixel color to the hardware.
-                        delay(3);
-                }
-                for (size_t i = 0; i < 50; i++) {
-                        pixels.setPixelColor(0, pixels.Color(-i,-i,i)); // Moderately bright green color.
-                        pixels.show(); // This sends the updated pixel color to the hardware.
-                        delay(3);
-                }
-                for (size_t i = 0; i < 50; i++) {
-                        pixels.setPixelColor(0, pixels.Color(i,-i,-i)); // Moderately bright green color.
-                        pixels.show(); // This sends the updated pixel color to the hardware.
-                        delay(3);
-                }
-
-
-
-}
-
-
-
-void loop() {
-
-        if (digitalRead(cmd)  == HIGH) {
-                int runonce = 0;
-                oled.clear();
-                oled.set2X();
-                oled.print("CMD");
-                oled.setCursor(0, 2);
-                //pulses = 0;
-
-                while(digitalRead(cmd)  == HIGH) {
-                        if (runonce == 0) {
-                                Serial.println("got cmd, attaching interupt");
-                                attachInterrupt(0, count_pulses, RISING);
-                                runonce =1;
-                                pulses = 0;
-                                pixels.setPixelColor(0, pixels.Color(0,0,150));
-                                pixels.show(); // This sends the updated pixel color to the hardware.
-                        }
-                }
-                oled.print(pulses);
-                Serial.println(pulses);
-
-                gotCMD = true;
-                detachInterrupt(0);
-                Serial.println("interupt detached");
-                for (int i = 0; i < pulses; i++) {
-
-                                pixels.setPixelColor(0, pixels.Color(0,0,150));
-                                pixels.show(); // This sends the updated pixel color to the hardware.
-                                delay(50);
-                                pixels.setPixelColor(0, pixels.Color(0,0,0));
-                                pixels.show(); // This sends the updated pixel color to the hardware.
-                                delay(50);
-                              }
-                run(pulses);
-        }
-        heartBeat();
-}
-
-void enc(){
-        pos++;
-}
-
-
-
-void heartBeat(){
-
-
-        if (millis() - heartBeatTimer > 20) {
-                pixels.setPixelColor(0, pixels.Color(0,beat,0)); // Moderately bright green color.
-                pixels.show(); // This sends the updated pixel color to the hardware.
-
-                if (beat > heartBeatMax && hearbeatDown == false) hearbeatDown = true;
-                if (beat < 10 && hearbeatDown == true) hearbeatDown = false;
-
-                if (hearbeatDown == false) {
-                        beat=beat+2;
-                }
-                else beat = beat -2;
-
-                heartBeatTimer = millis();
-                //  Serial.print("beat: "); Serial.println(beat);
-        }
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_MCP4725.h>
+#include <Adafruit_INA219.h>
+#include <enc.h>
 
 
 
 
-}
-void run(int steps){
-        pixels.setPixelColor(0, yellow); // Moderately bright green color.
-        pixels.show(); // This sends the updated pixel color to the hardware.
 
-        attachInterrupt(1, enc, RISING);
+void Motor1_enc_ISR();
+void Motor2_enc_ISR();
+void Display();
+//Pins:
 
-        steps = 3250*1.25;
-        fram();
-        while ( pos - steps < 0) {
+int motor2_input1 = PA8;
+int motor2_input2 = PA10;
 
 /*
-                Serial.print(pos);
-                Serial.print(" of ");
-                Serial.println(steps);
+   int step_M1 = PA1;
+   int step_M2 = PA2;
+   int cmd_M1 = PA3;
+   int cmd_M2 = PA4;
  */
+
+
+//Golbals:
+
+int LCDUpdateTimer =0;
+int runonce =12;
+int pulses =12;
+int motor2speed =0;
+
+void Motor1_enc_ISR();
+void Motor2_enc_ISR();
+void GotCommand();
+void Rot_enc_ISR();
+void run();
+
+
+LiquidCrystal_I2C lcd(0x3f,20,4);
+Adafruit_MCP4725 dac;
+Adafruit_INA219 ina219;
+
+void setup(){
+        pinMode(motor1_Enc_A, INPUT);
+        pinMode(motor1_Enc_B, INPUT);
+        pinMode(motor2_Enc_A, INPUT);
+        pinMode(motor2_Enc_B, INPUT);
+        pinMode(rot_EncA, INPUT);
+        pinMode(rot_EncB, INPUT);
+        pinMode(rot_EncBTN, INPUT);
+        pinMode(motor2_input1, PWM);
+        pinMode(motor2_input2, PWM);
+
+        lcd.init();            // initialize the lcd
+        lcd.backlight();
+        // Initialize the INA219.
+        // By default the initialization will use the largest range (32V, 2A).  However
+        // you can call a setCalibration function to change this range (see comments).
+        ina219.begin();
+        // To use a slightly lower 32V, 1A range (higher precision on amps):
+        //ina219.setCalibration_32V_1A();
+        // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+        ina219.setCalibration_16V_400mA();
+        dac.begin(0x62);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("BusV    Bus Current");
+
+        attachInterrupt(motor1_Enc_A, Motor1_enc_ISR, FALLING);
+        attachInterrupt(motor2_Enc_A, Motor2_enc_ISR, FALLING);
+        attachInterrupt(rot_EncA, Rot_enc_ISR, FALLING);
+
+        LCDUpdateTimer =millis();
+        dac.setVoltage(4095/4, 0);
+        pwmWrite(motor2_input1, 0);
+        pwmWrite(motor2_input2, 0);
+        //analogWrite(motor2_input1, 150);
+
+}
+void loop(){
+
+        if (millis() - LCDUpdateTimer >=100) {
+                Display();
+                LCDUpdateTimer=millis();
         }
 
-        off();
-        detachInterrupt(1);
-        pos =0;
-        gotCMD = false;
-        distance=distance+steps;
-        Serial.print("distance: ");
-        Serial.println(distance);
-        pixels.setPixelColor(0, pixels.Color(0,0,0));
-        pixels.show(); // This sends the updated pixel color to the hardware.
-        delay(50);
-        pixels.setPixelColor(0, yellow); // Moderately bright green color.
-        pixels.show(); // This sends the updated pixel color to the hardware.
-        delay(50);
-        pixels.setPixelColor(0, pixels.Color(0,0,0));
-        pixels.show(); // This sends the updated pixel color to the hardware.
-        delay(50);
-        pixels.setPixelColor(0, yellow); // Moderately bright green color.
-        pixels.show(); // This sends the updated pixel color to the hardware.
-        delay(50);
-        pixels.setPixelColor(0, pixels.Color(0,0,0));
-        pixels.show(); // This sends the updated pixel color to the hardware.
+
+        //motor2speed = rot_enc*100;
+        if (motor2speed > 0) {
+                run();
+        }
+
+
 }
 
-void count_pulses(){
 
-        pulses++;
+void run(){
+        enc_Motor2 =0;
+        motor2speed=0;
+
+        while (enc_Motor2 > -10000) {
+
+                if (millis() - LCDUpdateTimer >=100) {
+                        Display();
+                        LCDUpdateTimer=millis();
+                }
+                pwmWrite(motor2_input1, motor2speed);
+                pwmWrite(motor2_input2, 0);
+        }
+        pwmWrite(motor2_input1, 0);
+        pwmWrite(motor2_input2, 0);
+        Display();
+        delay(2000);
+        lcd.clear();
+
+        enc_Motor2=0;
+        Display();
+
+        pwmWrite(motor2_input1, 0);
+        pwmWrite(motor2_input2, motor2speed);
+        while (enc_Motor2 < 10000) {
+
+                if (millis() - LCDUpdateTimer >=100) {
+                        Display();
+                        LCDUpdateTimer=millis();
+                }
+        }
+        pwmWrite(motor2_input1, 0);
+        pwmWrite(motor2_input2, 0);
+        delay(2000);
+        rot_enc=0;
+        motor2speed=0;
+
 }
-void fram(){
-        digitalWrite(en, HIGH);
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
+
+
+void Display(/* arguments */) {
+        lcd.setCursor(0, 1);
+        lcd.print(ina219.getBusVoltage_V());
+        lcd.setCursor(13, 1);
+        lcd.print(ina219.getCurrent_mA());
+
+
+        lcd.setCursor(0,2);
+        lcd.print(enc_Motor1);
+        lcd.setCursor(6,2);
+        lcd.print(enc_Motor2);
+        lcd.setCursor(10,3);
+        lcd.print(motor2speed);
+        motor2speed = rot_enc*1000;
+
+/*
+        if (enc_Motor2 != enc_Motor2_OLD && enc_Motor2 >=0) {
+                dac.setVoltage(enc_Motor2,0);
+        }
+ */
 }
-void bak(){
-        digitalWrite(en, HIGH);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-}
-void off(){
-        digitalWrite(en, HIGH);
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, HIGH);
-        delay(100);
-        digitalWrite(en, LOW);
+/*
+   void GotCommand() {
+        while(digitalRead(cmd_M1)  == HIGH) {
+                if (runonce == 0) {
+                        Serial.println("got cmd, attaching interupt");
+                        attachInterrupt(0, count_pulses, RISING);
+                        runonce =1;
+                        pulses = 0;
+                        pixels.setPixelColor(0, pixels.Color(0,0,150));
+                        pixels.show(); // This sends the updated pixel color to the hardware.
+                }
+        }
+        lcd.print(pulses);
+        Serial.println(pulses);
+
+        gotCMD = true;
+        detachInterrupt(0);
+        Serial.println("interupt detached");
+        for (int i = 0; i < pulses; i++) {
+
+                pixels.setPixelColor(0, pixels.Color(0,0,150));
+                pixels.show();   // This sends the updated pixel color to the hardware.
+                delay(50);
+                pixels.setPixelColor(0, pixels.Color(0,0,0));
+                pixels.show();   // This sends the updated pixel color to the hardware.
+                delay(50);
+        }
+        run(pulses);
+   }
+
+   }
+ */
+
+
+
+
+void Menu(){
+
+
+        int marker = 0;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("    Main menu");
+        lcd.setCursor(1, 1);
+        lcd.print("Motor options");
+        lcd.setCursor(1, 2);
+        lcd.print("Input options");
+
+
 
 }
